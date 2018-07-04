4c33d317bad3c7de8803c153a07aabee01716509
xl <202481793@qq.com>
2018-07-04 16:46:24 +0800

    匿名函数

---
+++ mytest/lambda.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/5 15:49
+# @Author  : xuanle
+# @FileName: lambda.py
+# @Software: PyCharm
+# @GitHub  ：https://github.com/LeShine
+
+
+# 匿名函数关键字 lambda
+
+num = lambda a, b: a + 6
+
+print(1, 2)
+
+(lambda mystr: print(mystr))("你好帅！")
+
+
+# 变长函数
+
+def addmore(*num):
+    sum = 0
+    for i in num:
+        sum += i
+    return sum
+
+
+print(addmore(1, 2), addmore(1, 2, 3, 4, 5))
